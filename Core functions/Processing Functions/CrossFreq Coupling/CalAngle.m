462c2b20ba72bb4e7f55b54476518172e5948f7c
Sina Tafazoli <tafazolisina@gmail.com>
2025-10-02 18:41:39 -0400

    Delete Core functions directory
    No need for this data

--- Core functions/Processing Functions/CrossFreq Coupling/CalAngle.m
+++
@@ -1,7 +0,0 @@
-function [RadAng,DegAng] = CalAngle(inputArg1)
-%CALANGLE Calculates angle in radian and degree
-% RadAng = atan(imag(inputArg1)./real(inputArg1));
-% DegAng = atand(imag(inputArg1)./real(inputArg1));
-
-end
-
--- Core functions/Processing Functions/DimReduction/DistFunExmp.m
+++
@@ -1,2 +0,0 @@
-function D2=DistFunExmp(ZI,ZJ)
-D2=rand(size(ZJ,1),1);
