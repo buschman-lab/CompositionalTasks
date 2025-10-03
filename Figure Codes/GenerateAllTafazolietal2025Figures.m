1f07f4a1e6fea5122c446f30975497dc37147635
Sina Tafazoli <tafazolisina@gmail.com>
2025-10-03 17:54:30 -0400

    Add files via upload

---
+++ Figure Codes/GenerateAllTafazolietal2025Figures.m
@@ -0,0 +1,13 @@
+function GenerateAllTafazolietal2025Figures(DataPath,FigSavePath)
+%GENERATEALLTAFAZOLIETAL2025FIGURES Summary of this function goes here
+%DataPath is the path to data, download them from https://figshare.com/projects/Building_compositional_tasks_with_shared_neural_subspaces/243491
+%FigSavePath is the path you want the resulting figures to be saved
+%   Detailed explanation goes here
+Figure1(DataPath,FigSavePath)
+Figure2(DataPath,FigSavePath)
+Figure3(DataPath,FigSavePath)
+Figure4(DataPath,FigSavePath)
+Figure5(DataPath,FigSavePath)
+SuppFigures(DataPath,FigSavePath)
+end
+
